e84b84e3971a47b4bab715a83836e1a98fee48b3
SomjiTripathi <101165040+SomjiTripathi@users.noreply.github.com>
2022-03-11 12:39:44 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,12 +1,26 @@
 public class hello
 {
-
-public static void main(String []args)
-{
-	System.out.println("Hello EVERYONE Indian");
-	
-}
-
-
-
+	public static void main(String []args)
+	{
+		System.out.println("Hello EVERYONE Indian");
+		private String fname;
+	private String lname;
+	public Student(String fname, String lname) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	}
 }
